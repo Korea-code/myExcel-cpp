d9260b66c5c2442a84046fec23f6c233c4f10b44
Korea-code <rlawlgy93@gmail.com>
2020-01-03 21:57:14 -0500

    Initial Commit

---
+++ myExell/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myExell
+//
+//  Created by 김지효 on 2020-01-03.
+//  Copyright © 2020 김지효. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
